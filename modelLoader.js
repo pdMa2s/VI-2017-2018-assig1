3089e4eca2c50a90aa2b327d1edfe86055c6cb6b
Pedro Matos <pedroguilhermematos@ua.pt>
2017-11-09 23:40:50 +0000

    model loader

---
+++ modelLoader.js
@@ -0,0 +1,34 @@
+var mixers = [];
+
+
+function addModel(model){
+  var loader = new THREE.JSONLoader();
+  loader.load( "three.js-master/examples/models/animated/"+ model+".js", function( geometry ) {
+    mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( {
+      vertexColors: THREE.FaceColors,
+      morphTargets: true
+    } ) );
+    mesh.scale.set( 1.5, 1.5, 1.5 );
+    scene.add( mesh );
+    mixers.push(new THREE.AnimationMixer( mesh ));
+    var clip = THREE.AnimationClip.CreateFromMorphTargetSequence( 'move', geometry.morphTargets, 30 );
+    mixers[mixers.length-1].clipAction( clip ).setDuration( 1 ).play();
+
+  } );
+}
+
+function addHorse(){
+  addModel("horse");
+}
+
+function addFlaming(){
+  addModel("flamingo");
+}
+
+function addParrot(){
+  addModel("parrot");
+}
+
+function addStork(){
+  addModel("stork");
+}
