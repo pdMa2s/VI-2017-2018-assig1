b0eaa857ca5351302dc04983b4543b270cabb8b3
Pedro Matos <pedroguilhermematos@ua.pt>
2017-11-29 00:46:11 +0000

    import option

--- SceneImporter.js
+++ SceneImporter.js
@@ -1,6 +1,6 @@
 function importScene(jsonString){
-    var scene = JSON.parse(jsonString);
-    var sceneObjs = scene.sceneObjects;
+    var sceneProperties = JSON.parse(jsonString);
+    var sceneObjs = sceneProperties.sceneObjects;
     for(let i = 0; i< sceneObjs.length; i++){
         let pos = position(sceneObjs[i].position.x, sceneObjs[i].position.y, sceneObjs[i].position.Z);
         addObj(sceneObjs[i].name , sceneObjs[i].scale, sceneObjs[i].url, sceneObjs[i].texture, pos);
