b58fa8c3c03c9af7a6904ccd246ab9377d6dcdc0
FilipaTavares <anafilipatavares@ua.pt>
2017-11-29 11:13:36 +0000

    fix

--- SceneImporter.js
+++ SceneImporter.js
@@ -35,15 +35,29 @@ function importAnimations(obj,animations) {
             ani.rotation(animations[i].animation.axis.x,animations[i].animation.axis.y, animations[i].animation.axis.z);
         }
         else {
+            var trajPos = [];
+            for(var j = 0; j < animations[i].animation.pos.length; j++ ) {
+                trajPos.push(new THREE.Vector3(animations[i].animation.pos[j].x, animations[i].animation.pos[j].y,
+                    animations[i].animation.pos[j].z));
+            }
+            console.log(trajPos);
 
 
+            //var line = generateTrajectoryLineFromJSON();
 
-            //ani.trajectory()
+            //ani.trajectory(trajPositions, line);
+
         }
         obj.addAnimation(ani);
         objCollection.addAnimation(obj.name, ani);
     }
 }
+
 function position(xCord, yCord, zCord) {
     return {x: xCord, y: yCord, z: zCord};
+}
+
+function generateTrajectoryLineFromJSON() {
+    var points = [];
+
 }
